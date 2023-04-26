5be3c63d5a45420c35a954321a13f2e651fb93eb
Bo Bayles <bbayles@gmail.com>
2023-04-26 11:45:58 -0500

    Restore py3/none/plat tag

--- setup.py
+++ setup.py
@@ -3,23 +3,36 @@ from os.path import exists
 
 from setuptools import Distribution, find_packages, setup
 
+try:
+    from wheel.bdist_wheel import bdist_wheel
+except ImportError:
+    bdist_wheel = object
+
 with open("README.rst") as f:
     long_description = f.read()
 
 data_files = []
 bin_files = []
 
+
 class ExtensionDistribution(Distribution):
     def has_ext_modules(*args, **kwargs):
         return True
 
 
+class PlatformWheel(bdist_wheel):
+    def get_tag(self, *args, **kwargs):
+        python, abi, plat = super().get_tag(*args, **kwargs)
+        return ('py3', 'none', plat)
+
+
 for license_path in ('docs/License.html', 'docs/LICENSE'):
-    if exists(license_path):
+    if bdist_wheel and exists(license_path):
         data_files.append(('docs', [license_path]))
         bin_files.extend(['MediaInfo.dll', 'libmediainfo.*', 'libzen.*'])
         break
 
+cmdclass = {'bdist_wheel': PlatformWheel} if bin_files else {}
 distclass = ExtensionDistribution if bin_files else Distribution
 
 
@@ -46,6 +59,7 @@ setup(
     install_requires=["importlib_metadata; python_version < '3.8'"],
     package_data={'pymediainfo': bin_files},
     distclass=distclass,
+    cmdclass=cmdclass,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3.7",
